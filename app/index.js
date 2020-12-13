d8a1343925d56667b1fb4e84150336332bb066a3
ramiro-c <rami992009@gmail.com>
2020-12-13 17:09:12 -0300

    agregue nodemon y el servidor ahora envia el index.html

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.use(express.json());
 app.use(express.urlencoded({
@@ -23,7 +24,7 @@ app.listen(3000, () => {
 
 //rutas
 app.get('/', (req, res) => {
-    res.send('principal');
+    res.sendFile(path.join(__dirname + '/view/index.html'));
 });
 
 app.use('/api/persona', require('./routes/persona'));
