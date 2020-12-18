234e993aa28503b5aa3d0829aeda2fafe80a4837
Florencia_Fried <florencia.d.fried@gmail.com>
2020-12-17 22:48:20 -0300

    Iniciamos el ver viajes

--- app/seeders/20201214175327-viaje.js
+++ app/seeders/20201214175327-viaje.js
@@ -1,44 +1,44 @@
-'use strict';
+"use strict";
 
 module.exports = {
-
   up: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('viaje', [
+    return queryInterface.bulkInsert("viaje", [
       {
-      id: '1',
-      estadoViajeId: '2',
-      fecha: '2019/12/27',
-      horaInicio: new Date(),
-      observacion: 'Va a ser un viaje de mas de 10hs, equipaje de hasta 10kg. Vamos a parar 2 veces en el camino para comprar comida y descansar.',
-      posibilidadEquipaje: true,
-      precioPorPersona: null,
-      precioTotal: 8999.99,
-      perfilConductorId:'2',
-      origenId:'2',
-      destinoId:'3',
-      createdAt: new Date(),
-      updatedAt: new Date()
+        id: "1",
+        estadoViajeId: "2",
+        fecha: "2019/12/27",
+        horaInicio: new Date(),
+        observacion:
+          "Va a ser un viaje de mas de 10hs, equipaje de hasta 10kg. Vamos a parar 2 veces en el camino para comprar comida y descansar.",
+        posibilidadEquipaje: true,
+        precioPorPersona: null,
+        precioTotal: 8999.99,
+        perfilConductorId: "2",
+        origenId: "2",
+        destinoId: "3",
+        createdAt: new Date(),
+        updatedAt: new Date(),
       },
       {
-      id: '2',
-      estadoViajeId: '1',
-      fecha: '2020/01/02',
-      horaInicio: new Date(),
-      observacion: 'Va a ser un viaje de alrededor de 2hs, equipaje de hasta 7kg. Vamos a parar 5 min a mitad de camino para comprar comida.',
-      posibilidadEquipaje: true,
-      precioPorPersona: null,
-      precioTotal: 4999.99,
-      perfilConductorId:'1',
-      origenId:'1',
-      destinoId:'2',
-      createdAt: new Date(),
-      updatedAt: new Date()
+        id: "2",
+        estadoViajeId: "1",
+        fecha: "2020/01/02",
+        horaInicio: new Date(),
+        observacion:
+          "Va a ser un viaje de alrededor de 2hs, equipaje de hasta 7kg. Vamos a parar 5 min a mitad de camino para comprar comida.",
+        posibilidadEquipaje: true,
+        precioPorPersona: null,
+        precioTotal: 4999.99,
+        perfilConductorId: "1",
+        origenId: "1",
+        destinoId: "2",
+        createdAt: new Date(),
+        updatedAt: new Date(),
       },
     ]);
   },
 
   down: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('viaje', null, {});
-  }
+    return queryInterface.bulkDelete("viaje", null, {});
+  },
 };
-
