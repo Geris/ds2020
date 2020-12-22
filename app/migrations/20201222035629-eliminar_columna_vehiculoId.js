dbc1fc233e12b684bd7297970a30c4d43ec693c9
glizarde <gerardo.a.l@hotmail.com>
2020-12-22 01:17:35 -0300

    modificacion campo vehiculoId

---
+++ app/migrations/20201222035629-eliminar_columna_vehiculoId.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction(t => {
+      return Promise.all([
+        queryInterface.removeColumn('viaje', 'vehiculoId', { transaction: t })
+      ]);
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
